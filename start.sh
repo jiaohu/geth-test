38e2f15534a65f15e17d8c3a71da7022d9479e6c
Ubuntu <ubuntu@ip-10-20-155-41.us-west-2.compute.internal>
2023-08-22 08:18:20 +0000

    shell change

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 export LD_LIBRARY_PATH=./target/release:$LD_LIBRARY_PATH
 
-./geth --datadir gethdata init genesis.json
+# ./geth --datadir gethdata init genesis.json
+# ./geth init genesis.json
 
 #start local eth
+# mv genesis.json gethdata/genesis.json
 export LD_LIBRARY_PATH=./target/release:$LD_LIBRARY_PATH
-nohup ./geth  --http  --http.corsdomain "*" --datadir "./gethdata" --networkid 1337 --http.port 8545 --http.addr "0.0.0.0" > gethdata/geth.log 2>&1 &
+nohup ./geth --dev --http  --networkid 1337 --http.api "eth,net,web3,personal,web3,miner" --http.port 8545 --http.addr "0.0.0.0" > gethdata/geth.log 2>&1 &
+# nohup ./geth --http  --http.corsdomain "*" --datadir gethdata --networkid 1337 --http.port 8545 --http.addr "0.0.0.0" > gethdata/geth.log 2>&1 &
 sleep 10
 curl --location --request POST 'localhost:8545'  --header 'Content-Type: application/json'  --data-raw '{	"jsonrpc":"2.0",	"method":"eth_blockNumber",	"params":[],	"id":1337}'
 sleep 10
